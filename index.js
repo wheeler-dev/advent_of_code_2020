4545b1210c2a66d22bfeaf5a5953d9f7def103ef
Florian Wieland <fwieland@spirit21.com>
2020-12-03 01:15:59 +0100

    added solution for day 2 used input reader for better readability

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const day_one = () => {
+const fs = require('fs');
 
-    const inputs = [1313, 1968, 1334, 1566, 820, 1435, 1369, 1230, 1383, 1816, 1396, 1974, 1911, 1989, 1824, 1430, 1709, 1204, 1792, 1800, 1703, 2009, 1467, 1400, 1315, 1985, 1598, 1215, 1574, 1770, 1870, 1352, 1544, 1339, 188, 1347, 1986, 2003, 1538, 1839, 1688, 1350, 1191, 1961, 1578, 1946, 1548, 1975, 1745, 1631, 1390, 1811, 1586, 1409, 247, 1600, 1565, 1929, 1854, 1602, 1773, 1815, 1887, 1689, 1266, 1573, 1534, 1939, 1909, 1273, 1386, 1713, 1268, 1611, 1348, 1478, 1857, 1916, 1113, 936, 1603, 1716, 1875, 1855, 1834, 1701, 1279, 1346, 1503, 1797, 1287, 1447, 1475, 1950, 1614, 1261, 1442, 1299, 1465, 896, 1481, 1804, 1931, 1849, 1675, 1726, 355, 1485, 1343, 1697, 1735, 1858, 1205, 1345, 1281, 253, 1808, 1557, 1964, 1771, 1891, 1583, 1896, 1398, 1930, 1258, 1338, 1208, 1328, 1493, 1963, 1374, 1212, 1223, 1501, 2004, 1591, 1954, 115, 1972, 1814, 1643, 1270, 1349, 1297, 1399, 1969, 1237, 1228, 1379, 1779, 1765, 1427, 1464, 1247, 1967, 1577, 1719, 1559, 1274, 1879, 1504, 1732, 1277, 1758, 1721, 1936, 1605, 1358, 1505, 1411, 1823, 1576, 1682, 1439, 1901, 1940, 1760, 1414, 1193, 1900, 1990, 1781, 1801, 1239, 1729, 1360, 1780, 1848, 1468, 1484, 1280, 1278, 1851, 1903, 1894, 1731, 1451, 549, 1570];
-    // const inputs = [1721, 979, 366, 299, 675, 1456];
+const day_one = () => {
 
     let solutionPartOne;
     let solutionPartTwo;
+
+    const stringInputs = fs.readFileSync('./inputs/day_1').toString().split('\n');
+    const inputs = stringInputs.map((input) => {
+        return parseInt(input);
+    });
 
     const desiredSum = 2020;
 
@@ -20,8 +24,59 @@ const day_one = () => {
             });
         });
     });
-    console.log('Solution Part 1: ', solutionPartOne);
-    console.log('Solution Part 2:', solutionPartTwo);
+    console.log('Day 1, Part 1: ', solutionPartOne);
+    console.log('Day 1, Part 2:', solutionPartTwo);
 };
 
-day_one();
+const day_two = () => {
+    let validPasswordsPart1 = 0;
+    let validPasswordsPart2 = 0;
+    // read input from file
+    const input = fs.readFileSync('./inputs/day_2').toString().split('\n');
+    // remove last empty line
+    input.pop();
+
+    input.forEach((line) => {
+        // string operations
+        let password = line.substr(line.indexOf(':') + 2, line.length);
+        let passwordCharArray = password.split('');
+        let policy = line.substr(0, line.indexOf(':'));
+        let upper = parseInt(line.substring(line.indexOf('-') + 1, policy.length - 2));
+        let lower = parseInt(line.substr(0, line.indexOf('-')));
+        let match = policy.charAt(policy.length - 1);
+
+        console.log('---------------------------------------------');
+        console.log('line', line);
+        console.log('password: ', password);
+        console.log('policy: ', policy);
+        console.log('upper: ', upper);
+        console.log('lower: ', lower);
+        console.log('match: ', match);
+
+        let occurrences = 0;
+        passwordCharArray.forEach((char, index) => {
+            if (char === match) {
+                occurrences += 1;
+            }
+        });
+        console.log('occurrences: ', occurrences);
+        console.log('---------------------------------------------');
+
+        // part 1
+        if ((lower <= occurrences) && (occurrences <= upper)) {
+            validPasswordsPart1 += 1;
+        }
+
+        // part 2
+        if (passwordCharArray[lower - 1] === match ^ passwordCharArray[upper - 1] === match) {
+            validPasswordsPart2 += 1;
+        }
+
+    });
+
+    console.log('Day 2, Part 1: ', validPasswordsPart1);
+    console.log('Day 2, Part 2: ', validPasswordsPart2);
+};
+
+// day_one();
+day_two();
